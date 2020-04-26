ad2da672ea3bc7fb617d6f73a50a82211190c6cf
Ziyuan Ma <ziyuanma000@gmail.com>
2020-04-25 21:27:17 -0700

    fix

--- ppo.py
+++ ppo.py
@@ -342,7 +342,7 @@ if __name__ == '__main__':
 
     mpi_fork(args.cpu)  # run parallel code with mpi
 
-    from spinup.utils.run_utils import setup_logger_kwargs
+    from run_utils import setup_logger_kwargs
     logger_kwargs = setup_logger_kwargs(args.exp_name, args.seed)
 
     ppo(lambda : gym.make(args.env), actor_critic=core.MLPActorCritic,
